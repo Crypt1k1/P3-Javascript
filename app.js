ce50fd608df74a0748a0fc9c4fdb3dabe187f55b
Daniel Stefanov <9015492@student.zadkine.nl>
2022-02-01 12:30:04 +0100

    commit on

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 console.log("Hello second world");
+var x = "Hello World";
+var y = "Goodbye World"
+
+let hot = "Salah";
+const NAME = "timon";
+
+$name = "Julian";
+console.log(x);
+
+//7 verschillende operators//
+
+let opr = 10 + 20;
+console.log(opr);
+
+let opt = 10 - 20;
+console.log(opt);
+
+let opg = 10 * 20;
+console.log(opg);
+
+let oph = 10 / 20;
+console.log(oph);
+
+let opp = 10 % 20;
+console.log(opp);
+
+let opm = 10;  opm--
+console.log(opm);
+
+let oppp = 10; oppp++;
+console.log(oppp);
